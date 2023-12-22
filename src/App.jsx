c883ff2a359784a1fd13e2789966126ca21f5fae
Jason Garcia <146444354+RustyDL@users.noreply.github.com>
2023-12-21 19:16:03 -0500

    another changes maybe works

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import React, { useState } from "react";
 import Authenticate from "./components/authenticate";
 import SignUpForm from "./components/SignUpForm";
+
 export default function App() {
+  const [token, setToken] = useState(null);
+
   return (
     <>
-      <Authenticate />
-      <SignUpForm />
+      <SignUpForm setToken={setToken} />
+
+      <Authenticate token={token} />
     </>
   );
 }
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function SignUpForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
-  const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup");
-  const result = await response.json();
-  console.log(result);
+
   async function handleSubmit(event) {
     event.preventDefault();
 
     try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST", // Assuming you are using POST for signup
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      );
+      const result = await response.json();
+      console.log(result);
+      // Handle the result as needed, e.g., setToken(result.token);
     } catch (error) {
       setError(error.message);
     }
@@ -37,9 +48,8 @@ export default function SignUpForm() {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          />
         </label>
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </>
   );
